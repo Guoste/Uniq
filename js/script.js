ed56866cb596a69ea0ee35b5e93d57ada21a146d
Guoste <guoste.klimiene@gmail.com>
2017-11-28 23:00:10 +0200

    lot of change

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-$(function () {
+$(function() {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 3,
+        spaceBetween: 3,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+
+    //initialize date picker
     $("#datepicker").datepicker({
         firstDay: 1,
         beforeShowDay: function(date) {
-            if(date.getDay() == 4 || date.getDay() == 6) {
+            if (date.getDay() == 4 || date.getDay() == 6) {
                 return [true, "highlighted", ''];
             } else {
                 return [true, '', ''];
             }
 
-       }
+        }
     });
 });
